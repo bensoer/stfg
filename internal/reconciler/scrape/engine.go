733d1400e95ede6033b821326d74f3e824524f99
Ben Soer <ben@soernet.ca>
2026-09-05 20:39:12 -0700

    feat(reconciler): update scrape reconciler for canonical types
    Rework Reconcile signature to accept ctx context.Context,
    flyerfinder.FlyerFinder, and *storage.JSONFileStorage. Delete
    ScrapeClient/StorageClient interfaces and RetailGroup types.
    Rename RetailGroupIsValid -> FlyerIsValid taking storage.Flyer.

--- internal/reconciler/scrape/engine.go
+++ internal/reconciler/scrape/engine.go
@@ -1,68 +1,72 @@
 package scrape
 
 import (
+	"context"
 	"fmt"
-	"stfg/internal"
 	"strings"
 	"time"
+
+	"stfg/internal"
+	"stfg/internal/flyerfinder"
+	"stfg/internal/storage"
 
 	"go.uber.org/zap"
 )
 
-func Reconcile(scrapeClient ScrapeClient, storageClient StorageClient, options ScrapeReconcilerOptions) error {
+func Reconcile(ctx context.Context, finder flyerfinder.FlyerFinder, store *storage.JSONFileStorage, options ScrapeReconcilerOptions) error {
 	log := zap.S() //.With("source", "ScrapeReconciler")
 
-	// iterate over storageClient RetailGroups and remove whats invalid
-	rtgs, err := storageClient.GetAllRetailGroups()
+	// iterate over store Flyers and remove whats invalid
+	flyers, err := store.GetAllRetailGroups()
 	if err != nil {
 		return err
 	}
-	for _, rtg := range rtgs {
-		retailGroupIsValid, err := RetailGroupIsValid(rtg)
+	for _, f := range flyers {
+		flyerIsValid, err := FlyerIsValid(f)
 		if err != nil {
 			return err
 		}
-		if !retailGroupIsValid {
+		if !flyerIsValid {
 
-			log.Infof("Retail Group %s - %s Is Invalid. Removing", rtg.Merchant, rtg.Name)
-			log.Infof("Removing All Retail Group Items For Invalid Retail Group %s - %s", rtg.Merchant, rtg.Name)
-			retailGroupItems, err := storageClient.GetRetailGroupItems(rtg.ID)
+			log.Infof("Flyer %s - %s Is Invalid. Removing", f.Merchant, f.Name)
+			log.Infof("Removing All Flyer Items For Invalid Flyer %s - %s", f.Merchant, f.Name)
+			items, err := store.GetRetailGroupItems(f.ID)
 			if err != nil {
 				return err
 			}
-			for _, retailGroupItem := range retailGroupItems {
-				storageClient.RemoveRetailGroupItem(retailGroupItem)
+			for _, item := range items {
+				store.RemoveRetailGroupItem(item)
 			}
-			err = storageClient.RemoveRetailGroup(rtg)
+			err = store.RemoveRetailGroup(f)
 			if err != nil {
 				return err
 			}
 
 		}
 	}
-	// All expired RetailGroups are now gone, so now we can add new stuff
+	// All expired Flyers are now gone, so now we can add new stuff
 
-	// iterate over retail groups
-	rtgs, err = scrapeClient.GetRetailGroups(options.PostalCode)
+	// iterate over flyers
+	flyers, err = finder.FindFlyers(options.PostalCode)
 	if err != nil {
 		return err
 	}
 
-	for _, retailGroup := range rtgs {
+	for _, flyer := range flyers {
 
-		// Validation check that we aren't being given invalid retail groups from the scrapClient
-		retailGroupIsValid, err := RetailGroupIsValid(retailGroup)
+		// Validation check that we aren't being given invalid flyers from the scrapClient
+		flyerIsValid, err := FlyerIsValid(flyer)
 		if err != nil {
 			return err
 		}
-		if !retailGroupIsValid {
+		if !flyerIsValid {
 			continue
 		}
 
-		// Filter only RetailGroups that we want to see
+		// Filter only Flyers that we want to see
 		found := false
 		for _, whitelist := range options.RetailGroupWhiteList {
-			fullName := fmt.Sprintf("%s %s", retailGroup.Merchant, retailGroup.Name)
+			fullName := fmt.Sprintf("%s %s", flyer.Merchant, flyer.Name)
 			if strings.Contains(fullName, whitelist) {
 				found = true
 				break
@@ -72,31 +76,31 @@ func Reconcile(scrapeClient ScrapeClient, storageClient StorageClient, options S
 			continue
 		}
 
-		// NOTE: We assume that no RetailItems can have different validity timelines then the parent RetailGroup
-		// All RetailItems are valid within the RetailGroups start and end dates.
+		// NOTE: We assume that no FlyerItems can have different validity timelines then the parent Flyer
+		// All FlyerItems are valid within the Flyers start and end dates.
 
-		// At this point we can gaurantee the retail group does not exist OR the retail group exists and is still valid
+		// At this point we can gaurantee the flyer does not exist OR the flyer exists and is still valid
 
 		// If it does not exist, add it!
-		if !storageClient.HasRetailGroup(retailGroup) {
-			log.Infof("Adding Retail Group \"%s - %s\" And Its Items", retailGroup.Merchant, retailGroup.Name)
-			err := storageClient.AddRetailGroup(retailGroup)
+		if !store.HasRetailGroup(flyer) {
+			log.Infof("Adding Flyer \"%s - %s\" And Its Items", flyer.Merchant, flyer.Name)
+			err := store.AddRetailGroup(flyer)
 			if err != nil {
 				return err
 			}
 
-			rtgis, err := scrapeClient.GetRetailGroupItems(retailGroup.ID)
+			items, err := finder.FindFlyerItems(flyer.ID)
 			if err != nil {
 				return err
 			}
 
-			for _, retailGroupItem := range rtgis {
-				hasRetailGroupItem, err := storageClient.HasRetailGroupItem(retailGroupItem)
+			for _, item := range items {
+				hasRetailGroupItem, err := store.HasRetailGroupItem(item)
 				if err != nil {
 					return err
 				}
 				if !hasRetailGroupItem {
-					err := storageClient.AddRetailGroupItem(retailGroupItem)
+					err := store.AddRetailGroupItem(item)
 					if err != nil {
 						return err
 					}
@@ -111,14 +115,13 @@ func Reconcile(scrapeClient ScrapeClient, storageClient StorageClient, options S
 	return nil
 }
 
-func RetailGroupIsValid(retailGroup RetailGroup) (bool, error) {
-
-	validFrom, err := internal.ParseDate(retailGroup.ValidFrom)
+func FlyerIsValid(flyer storage.Flyer) (bool, error) {
+	validFrom, err := internal.ParseDate(flyer.ValidFrom)
 	if err != nil {
 		return false, err
 	}
 
-	validTo, err := internal.ParseDate(retailGroup.ValidTo)
+	validTo, err := internal.ParseDate(flyer.ValidTo)
 	if err != nil {
 		return false, err
 	}
